1d76f885215a99b8a30a22da08ae20375047ec53
Erich Kuba <ekuba@appsolute.nz>
2020-11-28 12:47:53 +1300

    Removed the lib folder as it is created by the build process

--- lib/highres.timer.d.ts
+++
@@ -1,6 +0,0 @@
-export declare type HighresTimeType = [number, number];
-declare const _default: {
-    (previousTimestamp?: HighresTimeType): HighresTimeType;
-    bigint(time?: [number, number]): bigint;
-};
-export default _default;
--- lib/index.d.ts
+++
@@ -1,4 +0,0 @@
-export { HighresTimeType } from './highres.timer';
-import { HighresTimeType } from './highres.timer';
-export declare function startTime(): HighresTimeType;
-export declare function elapsedTime(startTime: HighresTimeType): number;
