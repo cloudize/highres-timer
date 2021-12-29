51968372e6a5185705c10443cf1cb8eed25da5b8
Erich Kuba <erich.kuba@api.games>
2021-12-29 13:09:07 +1300

    Updated dependencies. Updated the license year. Bumped the version and published.

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,15 @@
-export { HighresTimeType } from './highres.timer';
 import highresTimer, { HighresTimeType } from './highres.timer';
+
+export { HighresTimeType } from './highres.timer';
 
 export function startTime(): HighresTimeType {
   return highresTimer();
 }
 
+// eslint-disable-next-line no-shadow
 export function elapsedTime(startTime: HighresTimeType): number {
-  const nanoSecondsPerSecond = 1000 * 1000 * 1000,
-    milliSecondsPerSecond = 1000 * 1000;
+  const nanoSecondsPerSecond = 1000 * 1000 * 1000;
+  const milliSecondsPerSecond = 1000 * 1000;
   const duration = highresTimer(startTime);
   return ((duration[0] * nanoSecondsPerSecond) + duration[1]) / milliSecondsPerSecond;
 }
